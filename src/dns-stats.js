5eed79e14913673bd945f85385dcaf8904b98145
Anton Ivanov <aivanov.mcu@gmail.com>
2022-05-03 02:25:53 +0300

    fix: dns-stats.js

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,8 +22,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(domains) {
-  let copy=[...domains];
+ function getDNSStats(domains) {
+  let regex = /[^.]+$/g;
+  let results=[...domains];
+  let obj={};
+  let partnext='';
+  while (results.length>0) {    
+    for (let i=0; i<results.length; i++) {if (!results[i].includes('.')) results[i]='.'+results[i]}
+    let part = (results.find(adress=>adress.match(regex)[0])).match(regex)[0];
+    obj[partnext+`.${part}`]=results.filter(adress => adress.includes(part)).length;
+    results=results.map(function(x){return x.replace(`.${part}`,'');}); // функция замены части строки в массиве строк при помощи метода map из стандарта ECMA-262
+    results=results.filter(adress=>adress.length>2);
+   // if (!results.every(elem=>elem.match(/[a-z][.]/))&&results.length!=1) partnext+=`.${part}`;
+    partnext+=`.${part}`;
+  }
+  return obj;
 }
 
 module.exports = {
